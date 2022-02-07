6aafddc27c71634afb78012283d8bf6e502dec4a
Douglas Zaltron <douglaszaltron@gmail.com>
2022-02-07 12:41:14 -0300

    fix: eslint

--- pages/api/v1/auth/index.js
+++ pages/api/v1/auth/index.js
@@ -1,22 +1,26 @@
 import app from '@/app';
 
 const auth = {
-  id: "5fca3821acf76c00202be101",
-  email: "douglas.zaltron@luizalabs.com",
-  username: "douglas.zaltron@luizalabs.com",
-  lastName: "Zaltron",
-  name: "Douglas",
-  sellers: [{
-    id: "5fca3d64e8720c9172fcc46d",
-    legalName: "Zaltron STORE SANDBOX LTDA",
-    tenantId: 'zaltron_store_sandbox',
-    services: [{
-      id: "product-api",
-      iconUrl: "",
-      name: "Catálogo",
-      url: "https://product-staging.luizalabs.com/"
-    }],
-  }],
+  id: '5fca3821acf76c00202be101',
+  email: 'douglas.zaltron@luizalabs.com',
+  username: 'douglas.zaltron@luizalabs.com',
+  lastName: 'Zaltron',
+  name: 'Douglas',
+  sellers: [
+    {
+      id: '5fca3d64e8720c9172fcc46d',
+      legalName: 'Zaltron STORE SANDBOX LTDA',
+      tenantId: 'zaltron_store_sandbox',
+      services: [
+        {
+          id: 'product-api',
+          iconUrl: '',
+          name: 'Catálogo',
+          url: 'https://product-staging.luizalabs.com/',
+        },
+      ],
+    },
+  ],
 };
 
 const handler = async (_request, response) => {
